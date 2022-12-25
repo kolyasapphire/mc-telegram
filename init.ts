51958bc2742e060ca1e0ae574f7ced3f39ff0a21
kolyasapphire <a@ks.gg>
2022-12-25 15:55:04 +0400

    Commands

--- init.ts
+++ init.ts
@@ -1,11 +1,19 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
+import 'dotenv/config'
 const { TOKEN, HOOK_URL } = process.env
+
+const commands = [{ command: 'check', description: 'Check who is playing.' }]
 
 const handler = async (_req: VercelRequest, res: VercelResponse) => {
   try {
     await fetch(
       `https://api.telegram.org/bot${TOKEN}/setWebhook?url=${HOOK_URL}`
+    )
+    await fetch(
+      `https://api.telegram.org/bot${TOKEN}/setMyCommands?commands=${JSON.stringify(
+        commands
+      )}`
     )
     res.end('Init successful')
   } catch {
